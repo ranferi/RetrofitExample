342dee11e51793405665ef26fca8499cf43701fc
Ranferi <ranferi.velo@gmail.com>
2018-05-02 20:47:52 -0500

    Okay

--- app/src/main/java/com/ranferi/retrofitexample/activities/HomeActivity.java
+++ app/src/main/java/com/ranferi/retrofitexample/activities/HomeActivity.java
@@ -1,15 +1,25 @@
 package com.ranferi.retrofitexample.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 
 import com.ranferi.retrofitexample.R;
+import com.ranferi.retrofitexample.helper.SharedPrefManager;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private TextView mTextViewName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +28,40 @@ public class HomeActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close
+        );
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        if (!SharedPrefManager.getInstance(this).isLoggedIn()) {
+            finish();
+            startActivity(new Intent(this, SignInActivity.class));
+        }
+        View headerView = navigationView.getHeaderView(0);
+        mTextViewName = (TextView) headerView.findViewById(R.id.text);
+        mTextViewName.setText(SharedPrefManager.getInstance(this).getUser().getName());
+
+        //loading home fragment by default
+        displaySelectedScreen(R.id.nav_home);
+
+
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
     }
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return false;
+    }
 }
--- app/src/main/java/com/ranferi/retrofitexample/activities/SignInActivity.java
+++ app/src/main/java/com/ranferi/retrofitexample/activities/SignInActivity.java
@@ -1,13 +1,26 @@
 package com.ranferi.retrofitexample.activities;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.ranferi.retrofitexample.R;
+import com.ranferi.retrofitexample.api.APIService;
+import com.ranferi.retrofitexample.api.APIUrl;
+import com.ranferi.retrofitexample.helper.SharedPrefManager;
+import com.ranferi.retrofitexample.model.Result;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class SignInActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -29,6 +42,47 @@ public class SignInActivity extends AppCompatActivity implements View.OnClickLis
 
     @Override
     public void onClick(View v) {
+        if (v == mButtonSignIn) {
+            userSignIn();
+        }
+    }
 
+    private void userSignIn() {
+        final ProgressDialog progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Signing Up...");
+        progressDialog.show();
+
+        String email = mEditTextEmail.getText().toString().trim();
+        String password = mEditTextPassword.getText().toString().trim();
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(APIUrl.BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        APIService service = retrofit.create(APIService.class);
+
+
+        Call<Result> call = service.userLogin(email, password);
+
+        call.enqueue(new Callback<Result>() {
+            @Override
+            public void onResponse(Call<Result> call, Response<Result> response) {
+                progressDialog.dismiss();
+                if (!response.body().getError()) {
+                    finish();
+                    SharedPrefManager.getInstance(getApplicationContext()).userLogin(response.body().getUser());
+                    startActivity(new Intent(getApplicationContext(), HomeActivity.class));
+                } else {
+                    Toast.makeText(getApplicationContext(), "Invalid email or password", Toast.LENGTH_LONG).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Result> call, Throwable t) {
+                progressDialog.dismiss();
+                Toast.makeText(getApplicationContext(), t.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
