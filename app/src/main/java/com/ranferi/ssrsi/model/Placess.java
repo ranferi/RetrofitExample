dd1772202cce6053f99056467b1a388d0219f55d
Ranferi <ranferi.velo@gmail.com>
2019-01-25 20:43:04 -0600

    lib externa

---
+++ app/src/main/java/com/ranferi/ssrsi/model/Placess.java
@@ -0,0 +1,20 @@
+package com.ranferi.ssrsi.model;
+
+import java.util.ArrayList;
+
+public class Placess {
+
+    private ArrayList<Places> placess;
+
+    public Placess() {
+
+    }
+
+    public ArrayList<Places> getPlacess() {
+        return placess;
+    }
+
+    public void setPlacess(ArrayList<Places> placess) {
+        this.placess = placess;
+    }
+}
