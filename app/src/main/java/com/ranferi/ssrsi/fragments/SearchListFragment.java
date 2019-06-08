83a53f031e27f837f71d465db91c0ef3034b487e
Ranferi <ranferi.velo@gmail.com>
2019-06-07 19:06:04 -0500

    En ApiService se cambio el tipo de obtencion de datos, en SearchFragment se cambiaron los autocomplete por spinners, se agregaron cadenas en los arrays en strings.xml

---
+++ app/src/main/java/com/ranferi/ssrsi/fragments/SearchListFragment.java
@@ -0,0 +1,30 @@
+package com.ranferi.ssrsi.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.ranferi.ssrsi.R;
+
+public class SearchListFragment extends Fragment {
+
+    public SearchListFragment() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_place_list, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+
+}
