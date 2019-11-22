04506964a554b2526c94c43440be847ee99c9d7d
Ranferi <ranferi.velo@gmail.com>
2019-11-21 20:15:11 -0600

    Se agregaron categorias a Types

--- app/src/main/java/com/ranferi/ssrsi/model/Types.java
+++ app/src/main/java/com/ranferi/ssrsi/model/Types.java
@@ -1,7 +1,28 @@
 package com.ranferi.ssrsi.model;
 
 public class Types {
-    public static final String[] strCatsNames = {"¿Qué tipo de lugar?", "Bar","Restaurante","Restaurante de curry japonés","Restaurante italiano"};
+    public static final String[] strCatsNames = {
+            "¿Qué tipo de lugar?",
+            "Bar","Restaurante",
+            "Restaurante de curry japonés",
+            "Restaurante italiano",
+            "Club nocturno",
+            "Club de rock",
+            "Local de sándwiches",
+            "Restaurante kurdo",
+            "Restaurante yucateco"
+    };
 
-    public static final String[] strCatsCodes = {"¿Qué tipo de lugar?", "Bar","Restaurante","Restaurante_de_curry_japones","Restaurante_italiano"};
+    public static final String[] strCatsCodes = {
+            "¿Qué tipo de lugar?",
+            "Bar",
+            "Restaurante",
+            "Restaurante_de_curry_japones",
+            "Restaurante_italiano",
+            "Club_nocturno",
+            "Club_de_rock",
+            "Local_de_sandwiches",
+            "Restaurante_kurdo",
+            "Restaurante_yucateco"
+    };
 }
